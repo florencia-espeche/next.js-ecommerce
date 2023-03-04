dcf9bfe8f363d02e6636cb10fe6fbe4a88720b06
Florencia <flor.espeche@gmail.com>
2023-03-04 18:22:56 -0300

    optimize stockPriceService code

--- pages/services/stockPriceService.ts
+++ pages/services/stockPriceService.ts
@@ -1,5 +1,6 @@
 import data from '../api/stock-price';
-async function getStockPrice(skuCode) {
+
+async function getStockPrice(skuCode: number): Promise<number | undefined> {
   if (skuCode && !isNaN(skuCode)) {
     return data[skuCode];
   }
